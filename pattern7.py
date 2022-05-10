64198108c4207f03dc84c3e492579264eb0a7628
Anurag <Anu9808@users.noreply.github.com>
2022-05-10 08:35:06 +0530

    adding pattern7.py

---
+++ pattern7.py
@@ -0,0 +1,20 @@
+#input= 5
+#output
+'''
+*********
+ *******
+  *****
+   ***
+    *
+'''
+
+n=int(input("Enter number of rows"))
+for row in range(n):
+
+    for space in range(row):
+        print(" ",end="")
+    
+    for col in range(2*(n-row)-1):
+        print("*", end="")
+
+    print()
